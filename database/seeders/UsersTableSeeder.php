93854e96ff7f33d21cab35d6a6749c4130e56dd7
muhamad salman fauzi <salmanfauzi0512@gmail.com>
2024-05-13 00:51:31 +0700

    create userseeder

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $admin = User::create([
+            'name' => 'Admin',
+            'email' => 'admin@mail.com',
+            'password' => Hash::make('password'),
+        ]);
+
+        // User seeder is omitted as per the prompt. Users should register themselves through the implemented API functionality.
+    }
+}
